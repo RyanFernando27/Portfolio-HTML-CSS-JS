3ba66d42f81601a1b9a8061ac9e08278f4d649c4
Ryan Fernando <138043611+RyanFernando27@users.noreply.github.com>
2024-10-30 00:23:41 +0530

    Adding small screen size navigation pane for all html pages and format pages using prettier

--- menu.js
+++ menu.js
@@ -1,11 +1,10 @@
-
-
-function menu(){
-    const content = document.getElementById('mobile-nav'); // Define 'content' properly
+// JavaScript function for menu appear for small screen sizes.
+function menu() {
+    const content = document.getElementById('mobile-nav'); 
     if (content) {
-      content.style.display = content.style.display === 'none' ? 'block' : 'none';
+        content.style.display = content.style.display === 'none' ? 'block' : 'none';
     } else {
-      console.error("Element with id 'content' not found");
+        console.error("Element with id 'content' not found");
     }
 }
-    
+
